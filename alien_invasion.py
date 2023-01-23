433795b54ffc869a98958e1cda3c5a6a4920d79f
86186 <1468578931@qq.com>
2023-01-23 21:29:37 +0800

    2

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,15 +15,32 @@ class AlienInvasion:
     def run_game(self):
         """  开始游戏的主循环"""
         while True:
-            #监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            #每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # 让最近绘制的屏幕可见：
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+    def _check_events(self):
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #向右移动飞船
+                    self.ship.moving_right = True
+                elif event.key ==pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+    def _update_screen(self):
+        # 每次循环时都重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # 让最近绘制的屏幕可见：
+        pygame.display.flip()
+
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
+        #飞船设置
+        self.ship_speed = 1.5
