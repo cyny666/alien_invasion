26ec978d95f750d020a2a0927b05b312476e8ff6
86186 <1468578931@qq.com>
2023-01-23 20:48:50 +0800

    1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,17 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
     def run_game(self):
         """  开始游戏的主循环"""
         while True:
@@ -14,7 +19,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            #让最近绘制的屏幕可见：
+            #每次循环时都重绘屏幕
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            # 让最近绘制的屏幕可见：
             pygame.display.flip()
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """储存游戏中所有设置的类"""
+    def __init__(self):
+        """初始化游戏的设置"""
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
